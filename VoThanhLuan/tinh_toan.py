df806c3c1742229acf503aaedbd209eebad6b4dd
Luan 31-10 <vluan9087@gmail.com>
2026-03-06 09:23:25 +0700

    bổ sung chức năng tính tổng cơ bản

---
+++ VoThanhLuan/tinh_toan.py
@@ -0,0 +1,9 @@
+# File tính toán
+def tinh_tong(a, b):
+    return a + b
+
+so_thu_nhat = 15
+so_thu_hai = 25
+ket_qua = tinh_tong(so_thu_nhat, so_thu_hai)
+
+print(f"Tổng của {so_thu_nhat} và {so_thu_hai} là: {ket_qua}")
