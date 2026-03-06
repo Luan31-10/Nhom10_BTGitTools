89c0d94c8fba42180b1678d1f758559ebcd64ee6
NguyenHongYenVy <nvy28101998@gmail.com>
2026-03-06 13:26:36 +0700

    Delete NguyenHongYenVy.py

--- NguyenHongYenVy.py
+++
@@ -1,9 +0,0 @@
-def kien_tra_chan_le(n):
-    if n % 2 == 0:
-        return "Số chẵn"
-    else:
-        return "Số lẻ"
-
-# Chạy thử chương trình
-number = int(input("Nhập một số bất kỳ: "))
-print(f"Kết quả: {kien_tra_chan_le(number)}")
