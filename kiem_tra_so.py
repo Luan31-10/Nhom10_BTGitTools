b8e5f911fb8f03fa980328f801af5d7248196ed6
LƯU THANH HUY <biluu174@gmail.com>
2026-03-06 10:30:32 +0700

    feat: cap nhat code cho phep nguoi dung nhap so tu ban phim
    UPDATE KIEM TRA SO CHAN LE

--- kiem_tra_so.py
+++ kiem_tra_so.py
@@ -1,5 +1,10 @@
-def kien_tra_chan_le(n):
-    if n % 2 == 0:
-        return "Số chẵn"
-    else:
-        return "Số lẻ"
+print("--- CHƯƠNG TRÌNH KIỂM TRA SỐ CHẴN LẺ ---")
+
+# Nhập số nguyên từ người dùng
+so_nhap_vao = int(input("Vui lòng nhập vào một số nguyên: "))
+
+# Kiểm tra chẵn lẻ và in ra kết quả
+if so_nhap_vao % 2 == 0:
+    print(f"=> Số {so_nhap_vao} là SỐ CHẴN.")
+else:
+    print(f"=> Số {so_nhap_vao} là SỐ LẺ.")
