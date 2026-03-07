d89f2d2a9aa578fce55011fda9f1bd40a24c0ebd
Luan 31-10 <vluan9087@gmail.com>
2026-03-07 22:18:11 +0700

    feat: hoàn thiện file gioi_thieu (Đã Amend)

--- VoThanhLuan/gioi_thieu.py
+++ VoThanhLuan/gioi_thieu.py
@@ -2,3 +2,4 @@
 print("Xin chào, mình là Võ Thành Luân!")
 print("Đây là tính năng thuộc nhánh feature/VoThanhLuan.")
 print("Nhiệm vụ: Cấu hình và thực hiện workflow cá nhân.")
+# Chỉnh sửa bằng Amend commit
